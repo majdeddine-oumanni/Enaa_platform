3a5acbe4ac56d7b69889b781835c7f96bb487ecf
majd eddine oumanni <majdeddineoumanni@gmail.com>
2025-07-10 09:47:27 +0100

    creating the structure with the service of Brief and Student

---
+++ Aprenant-Service/src/test/java/com/aprenant/aprenantservice/AprenantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aprenant.aprenantservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprenantServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ Brief-Service/src/test/java/com/brief/briefservice/BriefServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brief.briefservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BriefServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
